fd963677d9adb23dc22358dceed30581b3eaa22e
Abdelrahmankhaled12 <abdelrahman.khaled1086@gmail.com>
2024-07-24 19:37:46 +0300

    up

--- js/home.js
+++ js/home.js
@@ -26,17 +26,18 @@ buttonArrowRight.addEventListener("click", () => { navigation("right") })
 
 // Function to handle carousel navigation
 const navigation = (dir) => {
-    const container = document.getElementById("carouselContainer"); // Getting reference to carousel container
-    console.log(container)
-    // Calculating scroll amount based on direction
-    const scrollAmount =
-        dir === "left"
-            ? container.scrollLeft - (container.offsetWidth + 8)
-            : container.scrollLeft + (container.offsetWidth + 8);
+    const containers = document.querySelectorAll(".carouselContainerBoxes"); // Getting reference to carousel container
 
-    // Smooth scrolling to the calculated scroll amount
-    container.scrollTo({
-        left: scrollAmount,
-        behavior: "smooth",
-    });
+    containers.forEach((container) => {
+        // Calculating scroll amount based on direction
+        const scrollAmount =
+            dir === "left"
+                ? container.scrollLeft - (container.offsetWidth + 5)
+                : container.scrollLeft + (container.offsetWidth + 5);
+        // Smooth scrolling to the calculated scroll amount
+        container.scrollTo({
+            left: scrollAmount,
+            behavior: "smooth",
+        });
+    })
 };
